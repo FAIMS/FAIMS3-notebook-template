c3590f81c533ceeaaabc6c8a65f13a848f50350e
Brian Ballsun-Stanton <Denubis@users.noreply.github.com>
2022-10-10 11:33:25 +1100

    fixing ./
    Signed-off-by: Brian Ballsun-Stanton <Denubis@users.noreply.github.com>

--- replaceNotebook.sh
+++ replaceNotebook.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 
 gitdir=$(basename "$(git rev-parse --show-toplevel)")
 jsonfile=$(find . -name "*.json" -a ! -name "secret*json" | head -n1 )
-projectname=$(echo -n "$gitdir" | sed -r 's#./FAIMS3-[^-]+-##g' | sed 's/_/ /g' | sed -r 's/^[_ ]//')
+projectname=$(echo -n "$gitdir" | sed -r 's#FAIMS3-[^-]+-##g' | sed 's/_/ /g' | sed -r 's/^[_ ]//')
 export PROJECT_KEY=$(echo "${projectname,,}" | sed -E "s/[^A-Za-z0-9]+/_/g" | sed -E "s/_+$//")
 echo "Using $gitdir with $jsonfile as $projectname ($PROJECT_KEY) onto: $(jq .url secret.json)"
 
